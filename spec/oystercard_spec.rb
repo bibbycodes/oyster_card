7275d6dafbae2cef3043cb36d8a054603454d7e6
Robert <bibbycodes@gmail.com>
2019-10-21 14:00:12 +0100

    adds class for oyster card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
-require 'oystercard'
+require 'oyster_card'
 
 describe OysterCard do
+  context "When using the OysterCard Class" do
+    it "should have a balance of zero by default" do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
