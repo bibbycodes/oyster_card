a6830f9a7373c5b341c2780cbca458e173c4d5aa
Robert <bibbycodes@gmail.com>
2019-10-22 12:26:51 +0100

    extracted Journey class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,7 @@
 require 'oyster_card'
 
 describe OysterCard do
-  let (:station) {double (:station)}
+  let (:origin) {double (:station)}
   let (:destination) {double (:destination)}
 
   context "When using the OysterCard Class" do
@@ -11,8 +11,8 @@ describe OysterCard do
         expect(subject.balance).to eq(0)
       end
 
-      it "should raise an error if balance is less than minimum amount" do
-        expect{subject.touch_in(station)}.to raise_error "Insufficient funds!"
+      it "should raise an error if balance is less than the minimum amount" do
+        expect{subject.touch_in(origin)}.to raise_error "Insufficient funds!"
       end
 
       it "should add specified amount of money" do
@@ -35,32 +35,32 @@ describe OysterCard do
 
       it "should deduct the cost of a journey after touching out" do
         subject.top_up(5)
-        subject.touch_in(station)
+        subject.touch_in(origin)
         expect {subject.touch_out(destination)}.to change{subject.balance}.by(-1)
       end
 
       it "should be able to touch in during travel" do
         subject.top_up(5)
-        subject.touch_in(station)
+        subject.touch_in(origin)
         expect(subject.in_journey?).to be(true)
       end
 
       it "should register the station when touching in" do
         subject.top_up(5)
-        subject.touch_in(station)
-        expect(subject.entry_station).to be(station)
+        subject.touch_in(origin)
+        expect(subject.entry_station).to be(origin)
       end
 
     end
 
     context "Touching Out" do
 
-      it "records start and end stations in journey history" do
+      it "records start and end origins in journey history" do
         subject.top_up(5)
-        subject.touch_in(station)
+        subject.touch_in(origin)
         subject.touch_out(destination)
         expect(subject.current_journey.destination).to be(destination)
-        expect(subject.current_journey.origin).to be(station)
+        expect(subject.current_journey.origin).to be(origin)
       end
 
       it "should be able to touch out during travel" do
@@ -68,9 +68,9 @@ describe OysterCard do
         expect(subject.in_journey?).to be(false)
       end
 
-      it "should reset the value for entry station when touching out" do
+      it "should reset the value for entry origin when touching out" do
         subject.top_up(5)
-        subject.touch_in(station)
+        subject.touch_in(origin)
         subject.touch_out(destination)
         expect(subject.entry_station).to be(nil)
       end
@@ -79,7 +79,7 @@ describe OysterCard do
 
     it "should save the journey history" do
       subject.top_up(5)
-      subject.touch_in(station)
+      subject.touch_in(origin)
       subject.touch_out(destination)
       expect(subject.journey_history).to include(subject.current_journey)
     end
